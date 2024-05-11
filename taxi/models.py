7997fa6d3f968262c90192768a567bbd235ca3fa
Artem Andriushchenko <link.by.artem@gmail.com>
2024-05-11 13:05:28 +0300

    solution

--- taxi/models.py
+++ taxi/models.py
@@ -1,14 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.urls import reverse
 
 
 class Manufacturer(models.Model):
     name = models.CharField(max_length=255, unique=True)
     country = models.CharField(max_length=255)
-
-
 class Driver(AbstractUser):
     license_number = models.CharField(max_length=255, unique=True)
+
+    def get_absolute_url(self):
+        return reverse("taxi:driver-detail", kwargs={"pk": self.id})
 
 
 class Car(models.Model):
@@ -17,3 +19,6 @@ class Car(models.Model):
         Manufacturer, on_delete=models.CASCADE, related_name="cars"
     )
     drivers = models.ManyToManyField(Driver, related_name="cars")
+
+    def get_absolute_url(self):
+        return reverse("taxi:car-detail", kwargs={"pk": self.id})
